ace55f878a23fe5cdf5fc2c58e109fca3e7c77d3
Michal Tomčo <tomco.mito@gmail.com>
2025-11-25 13:23:41 +0100

    Implement rotational_cipher excercise (#15)

---
+++ rotational-cipher/rotational_cipher.py
@@ -0,0 +1,10 @@
+def rotate(text: str, key: int) -> str:
+    result = []
+    for char in text:
+        if char.islower():
+            result.append(chr((ord(char) - ord('a') + key) % 26 + ord('a')))
+        elif char.isupper():
+            result.append(chr((ord(char) - ord('A') + key) % 26 + ord('A')))
+        else:
+            result.append(char)
+    return ''.join(result)
